cc478d14dad431cb5d876469c92fa5802f22b69f
Stephan Avenwedde <StephanA@beckhoff.com>
2022-03-17 07:08:47 +0100

    initial commit

---
+++ DoxygenSample/DoxygenSample.cpp
@@ -0,0 +1,12 @@
+﻿// DoxygenSample.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "DoxygenSample.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
