d0234664f8e10e4262a3df8c5e1da71a7c5e8659
Stephan Avenwedde <s.avenwedde@gmail.com>
2022-03-25 15:57:06 +0100

    doxyfile added

--- ByteStream/ByteStream.h
+++ ByteStream/ByteStream.h
@@ -20,6 +20,17 @@
 #include <cstring>
 #include <algorithm>
 
+
+/*!
+ * A brief history of JavaDoc-style (C-style) comments.
+ *
+ * This is the typical JavaDoc-style C-style comment. It starts with two
+ * asterisks.
+ *
+ * @param theory Even if there is only one possible unified theory. it is just a
+ *               set of rules and equations.
+ */
+
 class ByteStream {
 
 public:
